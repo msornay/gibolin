af745d8ec8c2cc8d11d6bb575f943b7406bc3d71
Charles Vauvarin <charles.vauvarin@gmail.com~>
2017-10-30 22:02:40 +0100

    Init firebase app

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gibolin
+
+## Installing the firebase app
+
+```
+$ sudo npm install -g firebase-tools
+$ firebase login
+$ firebase deploy
+```
---
+++ static/js/config.js
@@ -0,0 +1,9 @@
+var config = {
+	apiKey: "AIzaSyB7jWZc7GO7HCxMExBiL8gsMNqIixanzVU",
+	authDomain: "gibolin-777b1.firebaseapp.com",
+	databaseURL: "https://gibolin-777b1.firebaseio.com",
+	projectId: "gibolin-777b1",
+	storageBucket: "gibolin-777b1.appspot.com",
+	messagingSenderId: "323847162066"
+};
+firebase.initializeApp(config);
