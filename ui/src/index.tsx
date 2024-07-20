e439b3e8e80312f7ae3ceef6b72b77098353bf8c
msy <msornay@gmail.com>
2024-07-20 22:33:00 +0200

    Table example in AppShell main

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -10,6 +10,7 @@ import {
     AppShell,
     Burger,
     MantineProvider,
+    Table,
 } from '@mantine/core';
 
 import '@mantine/core/styles.css'
@@ -18,6 +19,39 @@ import '@mantine/core/styles.css'
 // XXX(msy) Logo next to Burger
 
 import { useDisclosure } from '@mantine/hooks';
+
+function References() {
+    const elements = [
+        { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+        { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+        { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+        { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+        { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+    ];
+
+    const rows = elements.map((element) => (
+        <Table.Tr key={element.name}>
+        <Table.Td>{element.position}</Table.Td>
+        <Table.Td>{element.name}</Table.Td>
+        <Table.Td>{element.symbol}</Table.Td>
+        <Table.Td>{element.mass}</Table.Td>
+        </Table.Tr>
+    ));
+
+    return (
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Element position</Table.Th>
+              <Table.Th>Element name</Table.Th>
+              <Table.Th>Symbol</Table.Th>
+              <Table.Th>Atomic mass</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+    );
+}
 
 function GibolinShell() {
     const [opened, { toggle }] = useDisclosure();
@@ -40,7 +74,9 @@ function GibolinShell() {
                 />
             </AppShell.Header>
             <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-            <AppShell.Main>Main</AppShell.Main>
+            <AppShell.Main>
+                <References />
+            </AppShell.Main>
         </AppShell>
     );
 }
